80da39ab171de692709a56cfbf62f84931ae38f7
snowcloud <derek@snowcloud.co.uk>
2010-08-09 18:29:31 +0100

    removed signals from urls, to end of models

--- urls.py
+++ urls.py
@@ -2,18 +2,18 @@
 
 """
 
-from django.db.models import signals
+# from django.db.models import signals
+from django.conf import settings
 from django.conf.urls.defaults import *
-from django.contrib.comments.models import Comment
+# from django.contrib.auth.models import User
+# from django.contrib.comments.models import Comment
 
-from ct_groups.models import CTGroup, email_comment
+from ct_groups.models import CTGroup #, email_comment, fix_open_id, email_unique
 from ct_groups.forms import CTPageForm
 from ct_groups.views import blog_new_post, blog_post_edit, group_detail, group_edit, group_note, \
     remove_editor, make_editor, moderate_accept, moderate_refuse, moderate_refuse_confirm, moderate_remove, \
     invite_member, accept_invitation, complete_invitation, register_invitee, invitation_remove
 from ct_groups.decorators import group_perm
-
-signals.post_save.connect(email_comment, sender=Comment)
 
 blog_view = group_perm('blog', 'r')
 blog_edit = group_perm('blog', 'w')
