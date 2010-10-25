2cef63ca3b4abf5a109cb7314e5fc0590643c4cb
snowcloud <derek@snowcloud.co.uk>
2010-10-25 22:01:27 +0100

    added setup info

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ct-groups',
+    version='0.1dev',
+    packages=['ct_groups'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.rst').read(),
+)
