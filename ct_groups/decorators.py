39f1f0ef4d642a64faecf917196774a1f9a8143e
snowcloud <derek@snowcloud.co.uk>
2010-12-14 19:18:21 +0000

    converted tabs to spaces

--- ct_groups/decorators.py
+++ ct_groups/decorators.py
@@ -14,38 +14,38 @@ CTGroup = get_model('ct_groups', 'ctgroup')
 
 
 def check_permission(user, group=None, perm_type=None, access=None ):
-	"""if group is None, will check NON_GROUP_PERMS
-	if group instance is not CTGroup, will return None
-	otherwise, will look up group permissions and compare to user"""
-	
-	from ct_groups.models import NON_GROUP_PERMS
-	if (group is None):
-		return access in NON_GROUP_PERMS.get(perm_type, [])
-	if (not isinstance(group, CTGroup)) or user is None:
-		return False
-	try:
-		perm = CTGroupPermission.objects.get(group=group, name=perm_type)
-	except CTGroupPermission.DoesNotExist:
-		return False
-	return perm.check_permission(user, access)
+    """if group is None, will check NON_GROUP_PERMS
+    if group instance is not CTGroup, will return None
+    otherwise, will look up group permissions and compare to user"""
+    
+    from ct_groups.models import NON_GROUP_PERMS
+    if (group is None):
+        return access in NON_GROUP_PERMS.get(perm_type, [])
+    if (not isinstance(group, CTGroup)) or user is None:
+        return False
+    try:
+        perm = CTGroupPermission.objects.get(group=group, name=perm_type)
+    except CTGroupPermission.DoesNotExist:
+        return False
+    return perm.check_permission(user, access)
 
 def group_perm(perm_type, access):
-	def _group_perm(fn):
-		def _check(request, *args, **kwargs):
-			user = request.user
-			group_slug = kwargs.get('group_slug', None)
-			try:
-				group = CTGroup.objects.get(slug=group_slug) 
-				if check_permission(user, group, perm_type, access):
-					# raise Exception('%s, %s, %s' % (group, perm_type, access))
-					return fn(request, *args, **kwargs)
-			except CTGroup.DoesNotExist:
-				pass
-			if request.POST:
-				return login(request)
-			else:
-				return HttpResponseRedirect('/accounts/login/?next=%s' % (request.path))
-			
-		return _check
-	# raise Exception('%s, %s' % (perm_type, access))
-	return _group_perm
+    def _group_perm(fn):
+        def _check(request, *args, **kwargs):
+            user = request.user
+            group_slug = kwargs.get('group_slug', None)
+            try:
+                group = CTGroup.objects.get(slug=group_slug) 
+                if check_permission(user, group, perm_type, access):
+                    # raise Exception('%s, %s, %s' % (group, perm_type, access))
+                    return fn(request, *args, **kwargs)
+            except CTGroup.DoesNotExist:
+                pass
+            if request.POST:
+                return login(request)
+            else:
+                return HttpResponseRedirect('/accounts/login/?next=%s' % (request.path))
+            
+        return _check
+    # raise Exception('%s, %s' % (perm_type, access))
+    return _group_perm
